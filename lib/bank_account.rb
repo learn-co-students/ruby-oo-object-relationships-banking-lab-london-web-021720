72a4f959d6b20f85ad8e4312d3828abab0204860
Ali G Junejo <aligohar_junejo@live.com>
2020-02-20 15:32:11 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
+
 class BankAccount
 
+    attr_reader :name
+
+    attr_accessor :balance, :status
+
+    def initialize(name, balance = 1000, status = "open")
+        @name = name
+        @balance = balance
+        @status = status
+    end
+
+    def deposit(deposit)
+        @balance += deposit
+    end
+
+    def display_balance
+    current_balance = self.balance
+    
+    return "Your balance is $#{current_balance}."
+    
+    end
+
+    def valid?
+        if status == "open" && balance > 0
+            return true
+        else 
+            false
+        end
+    end
+
+    def close_account
+        @status = "closed"
+    end
+
 end
+
